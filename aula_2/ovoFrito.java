82f2649fc6f21bc4db82ab161b9c682624247a1f
jose.ccna <jose.ccna@gmail.com>
2025-07-15 16:53:58 -0300

    arquivo java ovo frito

--- aula_2/ovoFrito.java
+++ aula_2/ovoFrito.java
@@ -2,12 +2,38 @@ package aula_2;
 
 public class ovoFrito {
     public static void main(String[] args) {
-     
-     
-     
-     
-     
+        // Declarando as variáveis de ingrediente e utensílio 
+        String ingredienteOvo = "ovo";
+        String ingredienteOleo = "óleo";
+        String ingredienteSal = "sal"; 
+        String ingredientePimenta = "pimenta do reino";
+        String utencilioFrigideira = "frigideira";
+        String utensilioEspatula = "espátula";
+        String utensilioPrato = "prato";
+        String utensilioColher = "colher de sopa";        
+
         System.out.println("Ovo frito é uma delícia!");
+        System.out.println("Para fazer ovo frito, você vai precisar dos seguintes passos:");
+        System.out.println("1. Pegue a " + utencilioFrigideira);
+        System.out.println("2. Coloque a " + utencilioFrigideira + " no fogão na primeira boca disponível");
+        System.out.println("3. Acenda a boca do fogão na qual se encontra a " + utencilioFrigideira);
+        System.out.println("4. Coloque uma " + utensilioColher + " de " + ingredienteOleo + " na " + utencilioFrigideira);
+
+        // Continuação dos passos
+        System.out.println("5. Pegue o " + ingredienteOvo + " e segure-o com cuidado");
+        System.out.println("6. Leve o " + ingredienteOvo + " para cima da " + utencilioFrigideira);
+        System.out.println("7. Pegue a " + utensilioEspatula);
+        System.out.println("8. Use a " + utensilioEspatula + " para bater suavemente no " + ingredienteOvo);
+        System.out.println("9. Bata até quebrar a casca do " + ingredienteOvo);
+        System.out.println("10. Despeje o " + ingredienteOvo + " na " + utencilioFrigideira + " e descarte a casca");
+        System.out.println("11. Coloque uma pitada (0.5g) de " + ingredienteSal + " sobre o " + ingredienteOvo);
+        System.out.println("12. Coloque uma pitada (0.5g) de " + ingredientePimenta + " em pó sobre o " + ingredienteOvo);
+        System.out.println("13. Acompanhe por um minuto e meio o " + ingredienteOvo + " fritando na " + utencilioFrigideira);
+        System.out.println("14. Após um minuto e meio, desligue a boca do fogão utilizada");
+        System.out.println("15. Retire o " + ingredienteOvo + " da " + utencilioFrigideira + " utilizando a " + utensilioEspatula);
+        System.out.println("16. Coloque o " + ingredienteOvo + " com a " + utensilioEspatula + " no " + utensilioPrato);
+
+        System.out.println("Pronto! Agora é só saborear seu delicioso ovo frito!");
     }
-    
 }
+

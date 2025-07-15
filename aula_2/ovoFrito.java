10c08903074cb420d1f49fdfebefcba8175b0bf1
jose.ccna <jose.ccna@gmail.com>
2025-07-15 15:58:17 -0300

    inicio da seguda parte da aual

---
+++ aula_2/ovoFrito.java
@@ -0,0 +1,13 @@
+package aula_2;
+
+public class ovoFrito {
+    public static void main(String[] args) {
+     
+     
+     
+     
+     
+        System.out.println("Ovo frito é uma delícia!");
+    }
+    
+}
